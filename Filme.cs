9962d6eca73c43aa195f644a6981613919f89f3d
Marcos Raniery <marcosranier34@gmail.com>
2026-06-17 19:48:41 -0300

    feat: cria classe Filme

---
+++ Filme.cs
@@ -0,0 +1,16 @@
+﻿public class Filme
+{
+    public string titulo;
+    public string genero;
+    public int anoLancamento;
+    public int classificacaoIndicativa;
+
+    public void ExibirInformacoes()
+    {
+        Console.WriteLine("\n=== Filme Cadastrado ===");
+        Console.WriteLine("Título: " + titulo);
+        Console.WriteLine("Gênero: " + genero);
+        Console.WriteLine("Ano de Lançamento: " + anoLancamento);
+        Console.WriteLine("Classificação Indicativa: " + classificacaoIndicativa);
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,2 +1,7 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Filme filme = new Filme();
+filme.titulo = "Avatar";
+filme.genero = "Ficção Científica";
+filme.anoLancamento = 2009;
+filme.classificacaoIndicativa = 12;
+
+filme.ExibirInformacoes();
