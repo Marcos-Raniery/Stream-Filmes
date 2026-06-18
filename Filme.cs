893f71601830f57c17f06e48242e53330ffe3750
Marcos Raniery Vieira <926038@senacminas.edu.br>
2026-06-18 19:53:35 -0300

    feat: adiciona regra classificação

--- Filme.cs
+++ Filme.cs
@@ -5,12 +5,21 @@
     public int anoLancamento;
     public int classificacaoIndicativa;
 
+    public string ObterClassificacaoIndicativa()
+    {
+        if (classificacaoIndicativa <= 0)
+        {
+            return "Livre";
+        }
+        return classificacaoIndicativa.ToString() + " anos";
+    }
+
     public void ExibirInformacoes()
     {
         Console.WriteLine("\n=== Filme Cadastrado ===");
         Console.WriteLine("Título: " + titulo);
         Console.WriteLine("Gênero: " + genero);
         Console.WriteLine("Ano de Lançamento: " + anoLancamento);
-        Console.WriteLine("Classificação Indicativa: " + classificacaoIndicativa);
+        Console.WriteLine("Classificação Indicativa: " + ObterClassificacaoIndicativa());
     }
 }
