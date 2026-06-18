b6174f3ee3a3e99e1c7c26b7a5d67627d1c12898
Marcos Raniery Vieira <926038@senacminas.edu.br>
2026-06-17 21:31:47 -0300

    feat: implementa entrada de dados do usuário

--- Program.cs
+++ Program.cs
@@ -1,7 +1,15 @@
 ﻿Filme filme = new Filme();
-filme.titulo = "Avatar";
-filme.genero = "Ficção Científica";
-filme.anoLancamento = 2009;
-filme.classificacaoIndicativa = 12;
+
+Console.Write("Título: ");
+filme.titulo = Console.ReadLine();
+
+Console.Write("Gênero; ");
+filme.genero = Console.ReadLine();
+
+Console.Write("Ano de lançamento: ");
+filme.anoLancamento = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Classificação Indicativa: ");
+filme.classificacaoIndicativa = Convert.ToInt32(Console.ReadLine());
 
 filme.ExibirInformacoes();
